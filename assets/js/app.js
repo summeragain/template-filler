de2190df4226a074fbad762ec8d18d462c6a63f4
summeragain <darya_yeremenko@yahoo.com>
2014-04-23 12:31:35 +0300

    angular

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+angular.module('TemplateFillerApp', ['TemplateFillerApp.controllers']);
---
+++ assets/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module("TemplateFillerApp.controllers", [])
+
+.controller("mainController", function($scope){
+  $scope.switchPage = function() {
+    alert('+');
+  }
+})
