f5140ee32890194be36f0da976c3f5c35d2449d0
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-04-30 17:24:42 +0300

    Editable dataset

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -17,8 +17,133 @@ angular.module('TemplateFillerApp.directives', [])
       ];
       
       $scope.getField = function(data, name) {
-        return data[name];
+        return data[name] === undefined ? ' ' : data[name];
       }
+
+      $scope.fieldEdited = function(index, newValue) {
+        var oldValue = $scope.header[index];
+
+        if(newValue === oldValue) {
+          return;
+        }
+
+        if($scope.header.indexOf(newValue) !== -1) {
+          // TODO: log error
+          console.log('duplicated field: ', newValue);
+          return;
+        }
+
+        console.log('field updated. was:', oldValue, 'now:', newValue);
+
+        $scope.data.forEach(function(element) {
+          var oldData = element[oldValue];
+
+          if(oldData !== undefined) {
+            element[newValue] = oldData;
+            delete element[oldValue];
+          }
+        });
+
+        $scope.header[index] = newValue;
+      }
+      $scope.fieldCreated = function(newValue) {
+        if(newValue === '') {
+          return '...';
+        }
+
+        if($scope.header.indexOf(newValue) !== -1) {
+          // TODO: log error
+          console.log('duplicated field: ', newValue);
+          return;
+        }
+
+        console.log('field created:', newValue);
+
+        $scope.header.push(newValue);
+        return '';
+      }
+      $scope.dataUpdated = function(item, name, value) {
+        item[name] = value;
+      }
+
+      $scope.createRow = function() {
+        $scope.data.push({ });
+      }
+    }
+  };
+}])
+
+.directive('editable', ['$timeout', function($timeout) {
+  return {
+    restrict: 'A',
+    scope: {
+      callback: '&editable',
+      placeholder: '='
+    },
+
+    link: function($scope, elem, attr) {
+      var updateBlur = function(event) {
+        if(elem.text() == '') {
+          elem.text($scope.placeholder || attr.staticPlaceholder);
+          return;
+        }
+
+        $timeout(function() {
+          var result = $scope.callback({ $value: elem.text() });
+          if(result === '') {
+            elem.text('...');
+          }
+        });
+      };
+
+      var updateKeydown = function(event) {
+        if(elem.text() == '') {
+          elem.trigger('blur');
+          return;
+        }
+
+        event.preventDefault();
+
+        $timeout(function() {
+          var result = $scope.callback({ $value: elem.text() });
+          if(result !== undefined) {
+            elem.text(result);
+          }
+        });
+      };
+
+      if(elem.text() === '') {
+        elem.text(attr.staticPlaceholder || '')
+      }
+
+      elem
+        .attr('contenteditable', true)
+        
+        .on('blur', function(event) {
+          updateBlur(event);
+        })
+
+        .on('focus', function(event) {
+          if(elem.text() == attr.staticPlaceholder) {
+            elem.text('');
+            return;
+          }
+        })
+
+        .on('keydown', function(event) {
+          if(event.ctrlKey || event.altKey) {
+            return;
+          }
+
+          switch(event.keyCode) {
+            case 13: // enter key
+            {
+              updateKeydown(event);
+              break;
+            }
+          }
+
+        });
     }
   };
 }])
